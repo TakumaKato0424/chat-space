cb250d41125ef87d1aef55cfe4d364d3b7756e0d
arutonico <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-04-23 00:00:14 +0900

    message_paramsを追加
    フォームで送られてきた、パラメータをmessage_paramsでまとめる

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,6 +4,11 @@ class MessagesController < ApplicationController
   def index
     @message = ""
   private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+  end
+
   def set_group
     @group = Group.find(params[:group_id])
   end
