9b42b2b23f5ff7b4674fed4c275f972f75d33aa2
arutonico <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-04-22 23:58:59 +0900

    private内にset_groupを定義するし、before_actionで実行する
    指定のgroup_idを持つ@groupをあらかじめ取得する

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,11 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
     @message = ""
+  private
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
+
