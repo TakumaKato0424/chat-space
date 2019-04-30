05a1b33086a16aea55f725f10b315d8589f55d3d
arutonico <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-04-30 18:25:40 +0900

    JSONをもとにHTMLを生成

--- app/assets/javascripts/send_message.js
+++ app/assets/javascripts/send_message.js
@@ -1,4 +1,21 @@
 $(function() {
+  function buildHTML(message) {
+    image = ( message.image ) ? `<img src="${message.image}">` : "";
+    var html = `<div class="message">
+                  <div class="upper-info clearfix">
+                    <p class="upper-info__user">
+                      ${message.name}
+                    </p>
+                    <p class="upper-info__date">
+                      ${message.created_at}
+                    </p>
+                  </div>
+                  <p class="message__text">${message.content}</p>
+                  ${image}
+                </div>`;
+    return html;
+  }
+
   $('#new-message').on('submit', function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,5 +27,10 @@ $(function() {
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+
+    .done(function(data) {
+      var html = buildHTML(data);
+      $('.messages').append(html);
     })
 });
