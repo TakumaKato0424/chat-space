5dfef0b926c77cef9b7f53f15cd841d701e51cec
arutonico <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-04-30 18:17:52 +0900

    Ajaxで非同期通信の設定

--- app/assets/javascripts/send_message.js
+++ app/assets/javascripts/send_message.js
@@ -1,4 +1,14 @@
 $(function() {
   $('#new-message').on('submit', function(e) {
+    e.preventDefault();
+    var formData = new FormData(this);
+    var message_create_url = $(this).attr('action')
+    $.ajax({
+      url: message_create_url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
     })
 });
