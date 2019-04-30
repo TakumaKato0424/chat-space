b3e011b2fd2ec9c1c49bb9460a8e3e66117b2f70
arutonico <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-04-30 18:18:50 +0900

    HTMLとJSONの場合で処理を分けるように設定

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -10,7 +10,10 @@ class MessagesController < ApplicationController
     @message = @group.messages.new(message_params)
     if @message.save
       flash[:notice] = "メッセージが送信されました。"
-      redirect_to group_messages_path(@group)
+      respond_to do |format|
+        format.html { redirect_to group_messages_path(@group) }
+        format.json
+      end
     else
       @messsages = @group.messages.includes(:user)
       flash.now[:alert] = "メッセージを入力してください。"
