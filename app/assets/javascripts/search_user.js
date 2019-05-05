7fddb14e9925ee9d3726ce58571b8cb2609b0829
arutonico <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-05-05 09:29:37 +0900

    ユーザ検索機能の実装
    ユーザ検索フォームにキーワードを入力した際にajaxによる非同期通信を行う

--- app/assets/javascripts/search_user.js
+++ app/assets/javascripts/search_user.js
@@ -1,5 +1,15 @@
 $(document).on('turbolinks:load',function(){
   $(function() {
 
+    $('#user-search-field').on('keyup', function(e) {
+      var input = $('#user-search-field').val();
+
+      $.ajax({
+        type: 'GET',
+        url: '/users',
+        data: { keyword: input },
+        dataType: 'json'
+      })
+    });
   });
 });
