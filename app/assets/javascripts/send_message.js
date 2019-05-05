7dddf975d6582a63f0263bc8d7800ef8e3be9a3d
arutonico <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-05-05 09:45:31 +0900

    修正 tubolinks:loadの追記

--- app/assets/javascripts/send_message.js
+++ app/assets/javascripts/send_message.js
@@ -1,50 +1,52 @@
-$(function() {
-  function buildHTML(message) {
-    image = ( message.image ) ? `<img src="${message.image}">` : "";
-    var html = `<div class="message">
-                  <div class="upper-info clearfix">
-                    <p class="upper-info__user">
-                      ${message.name}
-                    </p>
-                    <p class="upper-info__date">
-                      ${message.created_at}
-                    </p>
-                  </div>
-                  <p class="message__text">${message.content}</p>
-                  ${image}
-                </div>`;
-    return html;
-  }
+$(document).on('turbolinks:load',function(){
+  $(function() {
+    function buildHTML(message) {
+      image = ( message.image ) ? `<img src="${message.image}">` : "";
+      var html = `<div class="message">
+                    <div class="upper-info clearfix">
+                      <p class="upper-info__user">
+                        ${message.name}
+                      </p>
+                      <p class="upper-info__date">
+                        ${message.created_at}
+                      </p>
+                    </div>
+                    <p class="message__text">${message.content}</p>
+                    ${image}
+                  </div>`;
+      return html;
+    }
 
-  function resetSendBTN(){
-    $('#new-message')[0].reset();
-    $('.new-message__submit-btn').attr('disabled',false);
-  }
+    function resetSendBTN(){
+      $('#new-message')[0].reset();
+      $('.new-message__submit-btn').attr('disabled',false);
+    }
 
-  $('#new-message').on('submit', function(e) {
-    e.preventDefault();
-    var formData = new FormData(this);
-    var url = $(this).attr('action');
-    $.ajax({
-      url: url,
-      type: "POST",
-      data: formData,
-      dataType: 'json',
-      processData: false,
-      contentType: false
-    })
+    $('#new-message').on('submit', function(e) {
+      e.preventDefault();
+      var formData = new FormData(this);
+      var url = $(this).attr('action');
+      $.ajax({
+        url: url,
+        type: "POST",
+        data: formData,
+        dataType: 'json',
+        processData: false,
+        contentType: false
+      })
 
-    .done(function(data) {
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      var position = $('.messages')[0].scrollHeight;
-      $('.messages').animate({scrollTop:position}, 500, 'swing');
-      resetSendBTN();
-    })
+      .done(function(data) {
+        var html = buildHTML(data);
+        $('.messages').append(html);
+        var position = $('.messages')[0].scrollHeight;
+        $('.messages').animate({scrollTop:position}, 500, 'swing');
+        resetSendBTN();
+      })
 
-    .fail(function() {
-      alert('メッセージを入力してください。');
-      resetSendBTN();
+      .fail(function() {
+        alert('メッセージを入力してください。');
+        resetSendBTN();
+      });
     });
   });
 });
