baaaa6b42c1b2bd617c7518b136b0e8b1ef64b18
arutonico <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-05-06 23:35:08 +0900

    indexアクションの追加
    DBの最新のメッセージのidと受信したメッセージのidを比較し、最新の状態でなければ@messagesを生成する
    
    この@messageを使って、クライアントのメッセージ一覧を自動更新
    する

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,6 @@
 class Api::MessagesController < ApplicationController
+  def index
+    # 送られてきたIDから最新のIDまでを取得させる
+    @messages = @group.messages.includes(:user).where("id > ?", params[:id])
+  end
 end
