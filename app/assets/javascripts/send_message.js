486ee9526a44f862a0b672915f076c556e244efc
arutonico <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-05-07 00:14:22 +0900

    ブラウザ上の最新のメッセージidを取得

--- app/assets/javascripts/send_message.js
+++ app/assets/javascripts/send_message.js
@@ -53,6 +53,8 @@ $(document).on('turbolinks:load',function(){
   var reloadMessages = function() {
     if(window.location.href.match(/\/groups\/\d+\/messages/)) {
       group_id = window.location.href.match(/groups\/(\d)/)[1];
+      //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+      last_message_id = $('.message:last').data('id');
   };
 
 });
