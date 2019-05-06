e304a324ea4b0c2dae570c3f254a25c45c96bbf3
arutonico <katoutakuma@katoutakumanoMacBook-Pro.local>
2019-05-06 23:49:51 +0900

    set_groupをbefore_actionに設定 追記
    @messagesに利用する為、あらかじめ設定

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,6 +1,10 @@
 class Api::MessagesController < ApplicationController
+  before_action :set_group
+
   def index
     # 送られてきたIDから最新のIDまでを取得させる
     @messages = @group.messages.includes(:user).where("id > ?", params[:id])
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
